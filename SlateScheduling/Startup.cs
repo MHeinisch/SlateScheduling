36f00a6c0b3c8ce024f6a3fb3e192e8b102a9017
Michael Heinisch <venomx411@gmail.com>
2018-02-24 11:20:58 -0600

    initial commit

---
+++ SlateScheduling/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SlateScheduling.Startup))]
+namespace SlateScheduling
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
